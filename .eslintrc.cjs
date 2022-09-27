9fba3a4d68db41af4f05b3068420c43207716e6c
Bruno Guimaraes <devgmrs@gmail.com>
2022-09-27 18:48:50 -0300

    :tada: init: Arquivos base do projeto. Stack: React, Vite, Tailwind, eslint, prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  plugins: [
+    'react', 'html',
+  ],
+  rules: {
+    'no-console': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': ['off'],
+  },
+};
