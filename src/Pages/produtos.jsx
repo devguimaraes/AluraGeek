614d9f2094cf628cb14c508a9c00c54ca4bd2818
Bruno Guimaraes <devgmrs@gmail.com>
2022-09-28 18:03:18 -0300

    :sparkles:	feat:  Add Product and Login page routes

---
+++ src/Pages/produtos.jsx
@@ -0,0 +1,3 @@
+export default function Produtos() {
+	return <h2>Produos</h2>;
+}
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './Pages/Home';
+import Login from './Pages/login';
+import Produtos from './Pages/produtos';
 
 export default function AppRouter() {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/produtos" element={<Produtos />} />
+				<Route path="/login" element={<Login />} />
 			</Routes>
 		</Router>
 	);
