831de25706be3ce2cb4ce61a727f8bb5b300b3ca
Bruno Guimaraes <devgmrs@gmail.com>
2022-09-29 00:50:58 -0300

    :recycle:	refactor: Acerto do Componente da rota Produtos

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,14 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './Pages/Home';
 import Login from './Pages/login';
-import Produtos from './Pages/produtos';
+import PageProdutos from './Pages/produtos';
 
 export default function AppRouter() {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="/produtos" element={<Produtos />} />
+				<Route path="/produtos" element={<PageProdutos />} />
 				<Route path="/login" element={<Login />} />
 			</Routes>
 		</Router>
