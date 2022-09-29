ff758003517609ede3f2041466b2cf7c93e40f08
Bruno Guimaraes <devgmrs@gmail.com>
2022-09-29 00:40:30 -0300

    :mag: melhorando SEO

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,17 @@
 import Header from '../../assets/Components/Header';
 import MenuNav from '../../assets/Components/Nav';
+import Produtos from '../../assets/Components/Produtos';
 
 export default function Home() {
 	return (
-		<header>
-			<MenuNav />
-			<Header />
-		</header>
+		<>
+			<header>
+				<MenuNav />
+				<Header />
+			</header>
+			<main>
+				<Produtos />
+			</main>
+		</>
 	);
 }
