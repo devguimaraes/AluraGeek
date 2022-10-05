f506a59b4f76d2650fa0f7cdb98741c5e1731142
Bruno Guimaraes <devgmrs@gmail.com>
2022-10-04 21:09:40 -0300

    :sparkles:	feat: Add Page Login

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,3 +1,46 @@
+import Contato from '../assets/Components/Contato';
+import MenuNav from '../assets/Components/Nav';
+
 export default function Login() {
-	return <h2>Login</h2>;
+	return (
+		<>
+			<MenuNav loginAtivo />
+			<div className="w-full flex justify-center text-center my-[90px]">
+				<div className="w-[275px] laptop:w-[423px] flex flex-col justify-center items-center ">
+					<h3 className="text-center text-[18px] text-corTituloProduto font-bold mb-8">
+						Iniciar Sessão
+					</h3>
+					<form action="">
+						<div>
+							<label htmlFor="email">
+								<input
+									type="text"
+									name="email"
+									className="w-[274px] laptop:w-[423px] py-3 gap-1"
+									placeholder="Escreva seu e-mail"
+								/>
+							</label>
+						</div>
+						<br />
+						<div>
+							<label htmlFor="senha">
+								<input
+									type="password"
+									name="senha"
+									className="border-b-[2px] w-[274px] laptop:w-[423px] py-3 gap-1"
+									placeholder="Digite sua senha"
+								/>
+							</label>
+						</div>
+					</form>
+					<button type="button" className="botaoAzul mt-8">
+						Enviar
+					</button>
+				</div>
+			</div>
+			<footer>
+				<Contato />
+			</footer>
+		</>
+	);
 }
