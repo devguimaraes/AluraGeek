d09aa88057644b7e4a30676c6d4f29be6b534168
Bruno Guimaraes <devgmrs@gmail.com>
2022-10-06 20:31:36 -0300

    :bug: fix: Acerto no input search do mobile que abria em outras telas.

--- src/assets/Components/Nav/InputSearchMobile/index.jsx
+++ src/assets/Components/Nav/InputSearchMobile/index.jsx
@@ -2,7 +2,7 @@ import { FcSearch } from 'react-icons/fc';
 
 export default function InputSearchMobile({ setOpenInput }) {
 	return (
-		<div className="z-10 flex flex-col bg-background-input rounded-lg items-center  tablet:flex ">
+		<div className="z-10 flex flex-col bg-background-input rounded-lg items-center  tablet:hidden ">
 			<input
 				className="bg-background-input gap-2 rounded-lg w-[300px] h-10 mx-auto text-[14px] font-semibold"
 				type="search"
